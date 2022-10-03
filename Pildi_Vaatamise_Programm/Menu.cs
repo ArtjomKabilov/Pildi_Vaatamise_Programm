9d7cf83c4f00cc9da6bceb2935acb9b247d0d71d
opilane <opilane@HP-CZC6297BB4>
2022-10-03 10:47:03 +0300

    do work 1 and 2, start work 3

---
+++ Pildi_Vaatamise_Programm/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pildi_Vaatamise_Programm
+{
+    public partial class Menu : Form
+    {
+        Button btn1, btn2, btn3;
+        public Menu()
+        {
+            this.Size = new Size(400, 400);
+            this.BackColor = Color.LightYellow;
+            btn1 = new Button()
+            {
+                Text = "Pildi vaatamise programm",
+                Location = new Point(40,150),
+                Size = new Size(100,50),
+                BackColor = Color.LightGreen,
+
+            };
+            btn2 = new Button()
+            {
+                Text = "Matemaatiline äraarvamismäng",
+                Location = new Point(140, 150),
+                Size = new Size(100, 50),
+                BackColor = Color.LightGreen
+
+
+            };
+            btn3 = new Button()
+            {
+                Text = "Sarnaste piltide leidmise mäng",
+                Location = new Point(240, 150),
+                Size = new Size(100, 50),
+                BackColor = Color.LightGreen
+
+
+            };
+            btn1.Click += Btn1_Click;
+            btn2.Click += Btn2_Click;
+            btn3.Click += Btn3_Click;
+            this.Controls.Add(btn1);
+            this.Controls.Add(btn2);
+            this.Controls.Add(btn3);
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            mang2 mang2 = new mang2();
+            mang2.ShowDialog();
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            mang mang = new mang();
+                mang.ShowDialog();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+    }
+}
--- Pildi_Vaatamise_Programm/mang.cs
+++ Pildi_Vaatamise_Programm/mang.cs
@@ -17,21 +17,19 @@ namespace Pildi_Vaatamise_Programm
     public partial class mang : Form
     {
         TableLayoutPanel tl;
-        NumericUpDown nud, nud2, nud3, nud4;
-        Label lbl5;
-        Button btn;
-        Label textLabel;
-        string text;
-        // Create a Random object called randomizer 
-        // to generate random numbers.
-        Random randomizer = new Random();
-
+        Random rnd = new Random();
+        char[] sümbolid = new char[] { '+', '-', '*', '/' };
+        int plussÜks, plussKaks;
+        int korrutadaÜks, korrutadaKaks;
+        int jagaÜks, jagaKaks;
+        int miinusÜks, miinusKaks;
+        int aega_jäänud;
         Timer timer;
-        Stopwatch sw;
+        Label lb;
+        Button start;
+
         public mang()
         {
-            string[] tehed = new string[4] { "+", "-", "/", "*" };
-
             this.Size = new Size(500, 400);
             this.Name = "Matemaatiline äraarvamismäng";
             tl = new TableLayoutPanel()
@@ -40,154 +38,216 @@ namespace Pildi_Vaatamise_Programm
                 AutoSize = true,
                 Location = new System.Drawing.Point(0, 0),
                 ColumnCount = 5,
-                RowCount = 5,
+                RowCount = 4,
                 TabIndex = 0,
                 BackColor = System.Drawing.Color.White
             };
-            /*
-                        tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                            (System.Windows.Forms.SizeType.Percent, 40F));
-                        tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                            (System.Windows.Forms.SizeType.Percent, 40F));
-                        tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                            (System.Windows.Forms.SizeType.Percent, 40F));
-                        tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                            (System.Windows.Forms.SizeType.Percent, 40F));
-                        tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                            (System.Windows.Forms.SizeType.Percent, 40F));
 
-                        tl.RowStyles.Add(new System.Windows.Forms.RowStyle
-                            (System.Windows.Forms.SizeType.Absolute, 40F));
-                        tl.RowStyles.Add(new System.Windows.Forms.RowStyle
-                            (System.Windows.Forms.SizeType.Absolute, 40F));
-                        tl.RowStyles.Add(new System.Windows.Forms.RowStyle
-                            (System.Windows.Forms.SizeType.Absolute, 40F));
-                        tl.RowStyles.Add(new System.Windows.Forms.RowStyle
-                            (System.Windows.Forms.SizeType.Absolute, 40F));*/
-            nud = new NumericUpDown()
+            tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 40F));
+            tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 40F));
+            tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 40F));
+            tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 40F));
+            tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 40F));
+
+            tl.RowStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Absolute, 40F));
+            tl.RowStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Absolute, 40F));
+            tl.RowStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Absolute, 40F));
+            tl.RowStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Absolute, 40F));
+
+            lb = new Label
             {
-                Font = new Font("Times New Roman", 18f),
-                Size = new Size(100, 40),
-                Name = "sum"
+                Font = new Font(Font.FontFamily, 18),
+                AutoSize = false,
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(200, 30),
             };
-            nud2 = new NumericUpDown()
+            Label label = new Label
             {
-                Font = new Font("Times New Roman", 18f),
-                Size = new Size(100, 40),
-                Name = "sum"
+                Font = new Font(Font.FontFamily, (float)15.75),
+                Text = "Aega jäänud",
+                AutoSize = true,
             };
-            nud3 = new NumericUpDown()
+            start = new Button
             {
-                Font = new Font("Times New Roman", 18f),
-                Size = new Size(100, 40),
-                Name = "sum"
+                Text = "Alustage viktoriini",
+                Font = new Font(Font.FontFamily, 14),
+                AutoSize = true,
+                TabIndex = 0
+                
             };
-            nud4 = new NumericUpDown()
+            start.Click += Start_Click;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+            for (int i = 1; i < 5; i++)
             {
-                Font = new Font("Times New Roman", 18f),
-                Size = new Size(100, 40),
-                Name = "sum"
-            };
-            var l_nimed = new string[5, 4];
-            for (int i = 0; i < 4; i++)
-            {
-                tl.RowStyles.Add(new System.Windows.Forms.RowStyle
-               (System.Windows.Forms.SizeType.Absolute, 40F));
-                for (int j = 0; j < 5; j++)
+                Label num1 = new Label
                 {
-                    tl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
-                    (System.Windows.Forms.SizeType.Percent, 40F));
-                    var l_nimi = "L" + j.ToString() + i.ToString();
-                    l_nimed[j, i] = l_nimi;
-                    if (j == 1) { text = tehed[i]; }
-                    else if (j == 3) { text = "="; }
-                    // else if (j == 4) { nud = new NumericUpDown { }; }
-                    else { text = "?"; }
-                    Label l = new Label { Text = text };
+                    Font = new Font(FontFamily.GenericSansSerif, 18),
+                    Text = "?",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Size = new Size(50, 60),
+                };
+                Label znak = new Label
+                {
+                    Font = new Font(FontFamily.GenericSansSerif, 18),
+                    Text = sümbolid[i - 1].ToString(),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Size = new Size(50, 60),
+                };
+                Label num2 = new Label
+                {
+                    Font = new Font(FontFamily.GenericSansSerif, 18),
+                    Text = "?",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Size = new Size(50, 60),
+                };
+                Label ravno = new Label
+                {
+                    Font = new Font(FontFamily.GenericSansSerif, 18),
+                    Text = "=",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Size = new Size(50, 60),
+                };
+                NumericUpDown X = new NumericUpDown
+                {
+                    Font = new Font(FontFamily.GenericSansSerif, 18),
+                    Width = 100,
+                    TabIndex = i + 1,
+                };
+                tl.Controls.Add(num1, 0, i);
+                tl.Controls.Add(znak, 1, i);
+                tl.Controls.Add(num2, 2, i);
+                tl.Controls.Add(ravno, 3, i);
+                tl.Controls.Add(X, 4, i);
+            }
+            tl.Controls.Add(lb, 3, 0);
+            tl.SetColumnSpan(label, 2);
+            tl.SetColumnSpan(lb, 2);
+            tl.Controls.Add(label, 1, 0);
 
-                    if (j == 4 && i == 0)
-                    {
-                        tl.Controls.Add(nud, j, i);
-                    }
-                    else if (j == 4 && i == 1)
-                    {
-                        tl.Controls.Add(nud2, j, i);
-                    }
-                    else if (j == 4 && i == 2)
-                    {
-                        tl.Controls.Add(nud3, j, i);
-                    }
-                    else if (j == 4 && i == 3)
-                    {
-                        tl.Controls.Add(nud4, j, i);
-                    }
-                    else
-                    {
-                        tl.Controls.Add(l, j, i);
-                    }
+            tl.SetColumnSpan(start, 2);
+            tl.Controls.Add(start, 2, 5);
+            Controls.Add(tl);
 
 
+        }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            NumericUpDown X = (NumericUpDown)tl.GetControlFromPosition(4, 1);
+            NumericUpDown minX = (NumericUpDown)tl.GetControlFromPosition(4, 2);
+            NumericUpDown mulX = (NumericUpDown)tl.GetControlFromPosition(4, 3);
+            NumericUpDown divX = (NumericUpDown)tl.GetControlFromPosition(4, 4);
+            if (CheckTheAnswer())
+            {
+                timer.Stop();
+                MessageBox.Show("Teil on kõik vastused õiged!",
+                                 "Palju õnne!");
+                start.Enabled = true;
+            }
+            else if (aega_jäänud > 0)
+            {
+                aega_jäänud = aega_jäänud - 1;
+                lb.Text = aega_jäänud + " sekundit";
+            }
+            else
+            {
+                timer.Stop();
+                lb.Text = "Aeg on läbi!";
+                MessageBox.Show("Sa ei lõpetanud õigeks ajaks.", "Vabandust!");
+                X.Value = plussÜks + plussKaks;
+                minX.Value = miinusÜks - miinusKaks;
+                mulX.Value = korrutadaÜks * korrutadaKaks;
+                divX.Value = jagaÜks / jagaKaks;
+                start.Enabled = true;
+            }
+        }
 
-                }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            start.Enabled = false;
+        }
+        private bool CheckTheAnswer()
+        {
+            NumericUpDown X = (NumericUpDown)tl.GetControlFromPosition(4, 1);
+            NumericUpDown minX = (NumericUpDown)tl.GetControlFromPosition(4, 2);
+            NumericUpDown mulX = (NumericUpDown)tl.GetControlFromPosition(4, 3);
+            NumericUpDown divX = (NumericUpDown)tl.GetControlFromPosition(4, 4);
+            if ((plussÜks + plussKaks == X.Value)
+                && (miinusÜks - miinusKaks == minX.Value)
+                && (korrutadaÜks * korrutadaKaks == mulX.Value)
+                && (jagaÜks / jagaKaks == divX.Value))
+                return true;
+            else
+                return false;
+        }
+
+        public void StartTheQuiz()
+        {
+            for (int row = 1; row < 5; row++)
+            {
+                Label num1 = (Label)tl.GetControlFromPosition(0, row);
+                Label symbol = (Label)tl.GetControlFromPosition(1, row);
+                Label num2 = (Label)tl.GetControlFromPosition(2, row);
+                NumericUpDown N = (NumericUpDown)tl.GetControlFromPosition(4, row);
+                int[] thing = getNums(symbol.Text);
+                num1.Text = thing[0].ToString();
+                num2.Text = thing[1].ToString();
+                N.Value = 0;
+            }
+            aega_jäänud = 30;
+            lb.Text = "30 sekundit";
+            timer.Start();
+        }
+
+        public int[] getNums(string summ)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            if (summ == "+")
+            {
+                num1 = rnd.Next(51);
+                num2 = rnd.Next(51);
+                plussÜks = num1;
+                plussKaks = num2;
+            }
+            else if (summ == "-")
+            {
+                num1 = rnd.Next(1, 101);
+                num2 = rnd.Next(1, num1);
+                miinusÜks = num1;
+                miinusKaks = num2;
+            }
+            else if (summ == "/")
+            {
+                num2 = rnd.Next(2, 11);
+                int temporaryQuotient = rnd.Next(2, 11);
+                num1 = num2 * temporaryQuotient;
+                jagaÜks = num1;
+                jagaKaks = num2;
+            }
+            else if (summ == "*")
+            {
+                num1 = rnd.Next(2, 11);
+                num2 = rnd.Next(2, 11);
+                korrutadaÜks = num1;
+                korrutadaKaks = num2;
             }
 
-
-            btn = new Button()
-            {
-                Font = new Font("Times New Roman", 15.75f),
-                Text = "Alusta",
-                Size = new Size(200, 60),
-                Location = new Point(100, 250),
-            };
-            lbl5 = new Label()
-            {
-                BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle,
-                AutoSize = false,
-                Size = new Size(200, 50),
-                Text = "asdasdsa",
-                Font = new Font("Times New Roman", 15.75f),
-                Location = new Point(150, 150),
-                Name = "timeLabel"
-            };
-            textLabel = new Label()
-            {
-                Font = new Font("Times New Roman", 15.75f),
-                Location = new Point(50, 150),
-                Name = "timeLabel",
-                Text = "Time Left"
-            };
-            btn.Click += Btn_Click;
-
-            //tl.Controls.Add(nud, 4, 4);
-
-
-            this.Controls.Add(lbl5);
-            this.Controls.Add(btn);
-            this.Controls.Add(textLabel);
-            this.Controls.Add(tl);
-        }
-        
-        private void Btn_Click(object sender, EventArgs e)
-        {
-            timer = new Timer();
-            timer.Interval = (30000);
-            timer.Tick += new EventHandler(timer_Tick);
-            sw = new Stopwatch();
-            timer.Start();
-            sw.Start();
-
-            // start processing emails
-
-            // when finished 
-            timer.Stop();
-            sw.Stop();
-            lbl5.Text = "Completed in " + sw.Elapsed.Seconds.ToString() + " seconds";
-        }
-        private void timer_Tick(object sender, EventArgs e)
-        {
-            lbl5.Text = "Running for " + sw.Elapsed.Seconds.ToString() + " seconds";
-            Application.DoEvents();
+            return new int[2] { num1, num2 };
         }
     }
 }
